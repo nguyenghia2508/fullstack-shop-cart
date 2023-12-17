11dc306f57a5d1109cd916e1e739fd4d8587269a
Nghia <binlatao3@gmail.com>
2023-12-17 19:35:33 +0700

    update V3

--- server/src/index.ts
+++ server/src/index.ts
@@ -29,12 +29,42 @@ const sessionConfig = {
   },
 };
 
+const corsOptions = {
+  origin: 'https://shop-cart-vercel.vercel.app/', // Thay thế bằng địa chỉ ứng dụng frontend của bạn
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+};
+
+const allowCors = (fn: (req: Request, res: Response) => Promise<void>) => async (req: Request, res: Response) => {
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
+  return await fn(req, res);
+};
+
+const handler = async (req: Request, res: Response) => {
+  const d = new Date();
+  res.end(d.toString());
+};
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname,"public")));
 app.use(cookieParser('ShopCart'));
 app.use(cors());
+app.use(allowCors(handler));
 app.use(function (request, response, next) {
   if (request.session && !request.session.regenerate) {
     request.session.regenerate = (cb: any) => {
