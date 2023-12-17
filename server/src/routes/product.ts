905e5042b420845a39da507ddea66f02dd46a458
Nghia <binlatao3@gmail.com>
2023-12-18 02:01:36 +0700

    update V30

--- server/src/routes/product.ts
+++ server/src/routes/product.ts
@@ -291,120 +291,115 @@ router.get('/:id/page/:page', async (req: Request, res: Response) => {
 
 router.post('/:id', async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const username = req.query.user as string;
-      const body = req.body;
-      const perPage = 4;
-      const page = 1;
-      const id = (req.params.id).replace(/-/g, ' ');
-      
-      const userExists = await User.exists({ username: username });
-      if (!body.rating) {
-        res.status(200).send({ code: '11', message: 'Please choose your rating' ,state:'false'});
-      }
-      else if (!userExists)
-      {
-        res.status(200).send({ code: '15', message: 'You need to login' ,state:'false'});
-      }
-      else {
-        const productExists = await Product.exists({ name: { $regex: `^${id}$`, $options: 'i' } });
-  
-        if (productExists) {
-          const ratingExists = await Rating.exists({ productName: { $regex: `^${id}$`, $options: 'i' } });
-  
-          if (ratingExists) {
-            const listReview = {
-              userReview: await nameUser(username),
-              reviewPost: body.reviewText,
-              rating: body.rating,
-              date: formatDate()
-            };
-  
-            const updateResult = await Rating.updateOne({ productName: { $regex: `^${id}$`, $options: 'i' } },
-             { $push: { listReview } }
-            );
-  
-            if (updateResult) {
-              const rt = await Rating.findOne({ productName: { $regex: `^${id}$`, $options: 'i' } }, { listReview: 1 });
-  
-              if (rt) {
-                const count = rt.listReview.length;
-                const totalPages: (number | string)[] = [];
-                const pageTotal = Math.ceil(count / perPage);
-                const currentpage = page;
-
-                if (pageTotal <= 6) {
-                    for (let i = 1; i <= pageTotal; i++) {
-                        totalPages.push(i);
-                    }
-                } else {
-                    totalPages.push(1);
-    
-                    if (currentpage > 3) {
-                        totalPages.push('...');
-                    }
-    
-                    if (currentpage === pageTotal) {
-                        totalPages.push(currentpage - 2);
-                    }
-    
-                    if (currentpage > 2) {
-                        totalPages.push(currentpage - 1);
-                    }
-    
-                    if (currentpage !== 1 && currentpage !== pageTotal) {
-                        totalPages.push(currentpage);
-                    }
-    
-                    if (currentpage < pageTotal - 1) {
-                        totalPages.push(currentpage + 1);
-                    }
-    
-                    if (currentpage === 1) {
-                        totalPages.push(currentpage + 2);
-                    }
-    
-                    if (currentpage < pageTotal - 2) {
-                        totalPages.push('...');
-                    }
-                    totalPages.push(pageTotal);
-                }
-            
-                const listReview: any[] = rt.listReview.map((lt: any) => ({
-                  userReview: lt.userReview,
-                  reviewPost: lt.reviewPost,
-                  rating: lt.rating,
-                  date: lt.date
-                }));
-            
-                const listReviewSlice = paginateArray(listReview, currentpage, perPage);
-                const totalRating = calculateRatingCounts(listReview).reverse();
-                const convertedTotalRating: { rating: string; count: number }[] = totalRating.map(item => ({
-                    rating: item.rating.toString(),
-                    count: item.count,
-                }));
-                const averageRating: string = (calculateAverageRating(convertedTotalRating).toFixed(1));
-                const [integerPart, decimalPart] = separateDecimal(parseFloat(averageRating));
-                res.status(200).send({ code: '10', message: 'Comment Success', 
-                    listReviewSlice,
-                    count, 
-                    totalPages,
-                    totalRating,
-                    averageRating,
-                    integerPart,
-                    decimalPart,
-                    state:'success'
-                });
-              } else {
-                res.status(404).send({ code: '404', message: 'Rating not found' ,state:'false'});
-              }
-            }
-          }
+        const username = req.query.user as string;
+        const body = req.body;
+        const perPage = 4;
+        const page = 1;
+        const id = (req.params.id).replace(/-/g, ' ');
+        
+        const userExists = await User.exists({ username: username });
+        if (!body.rating) {
+            res.status(200).send({ code: '11', message: 'Please choose your rating' ,state:'false'});
         }
-        else
+        else if (!userExists)
         {
-            res.status(200).send({ code: '13', message: 'Product not exists' ,state:'false'});
+            res.status(200).send({ code: '15', message: 'You need to login' ,state:'false'});
         }
-      }
+
+        const product = await Product.findOne({ name: { $regex: `^${id}$`, $options: 'i' } });
+
+        if (!product) {
+            res.status(200).send({ code: '13', message: 'Product not exists', state: 'false' });
+        return;
+        }
+
+        const rating = await Rating.findOneAndUpdate(
+            { productName: { $regex: `^${id}$`, $options: 'i' } },
+            {
+              $push: {
+                listReview: {
+                  $each: [{ userReview: await nameUser(username), reviewPost: body.reviewText, rating: body.rating, date: formatDate() }],
+                  $position: 0, 
+                },
+              },
+            },
+            { new: true } // Return the updated document
+          ).select('listReview');
+      
+          if (!rating) {
+            res.status(404).send({ code: '404', message: 'Rating not found', state: 'false' });
+            return;
+        }
+      
+        const count = rating.listReview.length;
+        const totalPages: (number | string)[] = [];
+        const pageTotal = Math.ceil(count / perPage);
+        const currentpage = page;
+
+        if (pageTotal <= 6) {
+            for (let i = 1; i <= pageTotal; i++) {
+                totalPages.push(i);
+            }
+        } else {
+            totalPages.push(1);
+
+            if (currentpage > 3) {
+                totalPages.push('...');
+            }
+
+            if (currentpage === pageTotal) {
+                totalPages.push(currentpage - 2);
+            }
+
+            if (currentpage > 2) {
+                totalPages.push(currentpage - 1);
+            }
+
+            if (currentpage !== 1 && currentpage !== pageTotal) {
+                totalPages.push(currentpage);
+            }
+
+            if (currentpage < pageTotal - 1) {
+                totalPages.push(currentpage + 1);
+            }
+
+            if (currentpage === 1) {
+                totalPages.push(currentpage + 2);
+            }
+
+            if (currentpage < pageTotal - 2) {
+                totalPages.push('...');
+            }
+            totalPages.push(pageTotal);
+        }
+
+        const listReview: any[] = rating.listReview.map((lt: any) => ({
+            userReview: lt.userReview,
+            reviewPost: lt.reviewPost,
+            rating: lt.rating,
+            date: lt.date
+          }));
+      
+          const listReviewSlice = paginateArray(listReview, currentpage, perPage);
+          const totalRating = calculateRatingCounts(listReview).reverse();
+          const convertedTotalRating: { rating: string; count: number }[] = totalRating.map(item => ({
+              rating: item.rating.toString(),
+              count: item.count,
+          }));
+          const averageRating: string = (calculateAverageRating(convertedTotalRating).toFixed(1));
+          const [integerPart, decimalPart] = separateDecimal(parseFloat(averageRating));
+          res.status(200).send({ code: '10', message: 'Comment Success', 
+              listReviewSlice,
+              count, 
+              totalPages,
+              totalRating,
+              averageRating,
+              integerPart,
+              decimalPart,
+              state:'success'
+          });
+  
+
     } catch (err) {
       console.log(err);
       res.status(500).send({ code: '500', message: 'Internal Server Error',state:'false'});
