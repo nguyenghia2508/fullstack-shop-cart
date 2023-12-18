137f17e07b6147ac59cbf0caea07f1c607d043c7
Nghia <binlatao3@gmail.com>
2023-12-18 16:36:23 +0700

    update V36

--- server/src/routes/product.ts
+++ server/src/routes/product.ts
@@ -320,11 +320,11 @@ router.post('/:id', async (req: Request, res: Response, next: NextFunction) => {
             date: formatDate()
           };
 
-        const updateRt = await Rating.updateOne({ productName: { $regex: `^${id}$`, $options: 'i' } },
+        const updateRating = await Rating.updateOne({ productName: { $regex: `^${id}$`, $options: 'i' } },
             { $push: { listReview } }
         );
 
-        if(updateRt)
+        if(updateRating)
         {
             const rt = await Rating.findOne({ productName: { $regex: `^${id}$`, $options: 'i' } }, { listReview: 1 });
             if(rt)
