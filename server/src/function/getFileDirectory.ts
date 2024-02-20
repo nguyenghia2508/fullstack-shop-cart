d6ecd62c10c7e667f1019c74d9013283ba25f7f3
Nghia <binlatao3@gmail.com>
2024-02-20 11:44:36 +0700

    update V89

--- server/src/function/getFileDirectory.ts
+++ server/src/function/getFileDirectory.ts
@@ -1,5 +1,5 @@
 import path from 'path';
 
 export async function getFileDirectory(): Promise<string> {
-  return path.join(process.cwd(), "/python/fin_and_agrawal.py");
+  return path.join(process.cwd(), "fin_and_agrawal.py");
 }
